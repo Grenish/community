f7fc5c81a60ad270dd5645a80ea4aa7e914fa444
Grenish rai <ariestheracer@gmail.com>
2024-04-14 20:30:15 +0530

    Add new colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#fbfefb",
+        secondary: "#f4f4f9",
+        heart: "#e63946",
+      }
+    },
   },
   plugins: [],
 };
