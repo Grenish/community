f06f484c842fd60ee456e38ebc4034095f8a63bb
Grenish rai <ariestheracer@gmail.com>
2024-04-14 20:30:48 +0530

    Add Feeds component

---
+++ src/components/Feeds.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+const Feeds = () => {
+  useEffect(() => {
+    document.title = "Feeds";
+  });
+
+  return (
+    <div>
+      <h1 className="">Feeds</h1>
+    </div>
+  );
+};
+
+export default Feeds;
