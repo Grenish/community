0129d37f7238adbd575de9f8d44bd26678443ebf
Grenish rai <ariestheracer@gmail.com>
2024-04-15 23:54:05 +0530

    Remove unused import statement in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import Feeds from "./components/Feeds";
 import Posts from "./components/Posts";
 import {
   SignedIn,
   SignedOut,
   SignInButton,
-  UserButton,
 } from "@clerk/clerk-react";
 
 function App() {
