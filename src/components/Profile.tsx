1b131537d00cf469560e6e5aad68cf0b6dc79546
Grenish rai <ariestheracer@gmail.com>
2024-04-20 20:05:55 +0530

    Add user profile image to Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,15 @@
+import { useUser } from "@clerk/clerk-react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 const Profile = () => {
+  const { user } = useUser();
   return (
     <div>
-      <h1>Profile</h1>
+      <img src={user?.imageUrl} alt="" />
     </div>
   );
 };
