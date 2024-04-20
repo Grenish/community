4a7075b3739a43adba324dda21a23cbe00d73d46
Grenish rai <ariestheracer@gmail.com>
2024-04-20 20:05:47 +0530

    Added routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import * as ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Profile from "./components/Profile.tsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/profile/:id",
+    element: <Profile />,
+  },
+]);
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
@@ -13,7 +26,7 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <RouterProvider router={router} />
     </ClerkProvider>
   </React.StrictMode>
 );
